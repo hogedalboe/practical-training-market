b55a5c93550682bfd4f56b1d84a79c9064fbc695
Nikolaj Høgedal Boe <45713352+hogedalboe@users.noreply.github.com>
2020-12-29 16:50:32 +0100

    Kruskal-Wallis

--- Python/11. Inferential statistics.py
+++ Python/11. Inferential statistics.py
@@ -7,7 +7,8 @@ import pandas
 import numpy as np 
 import scipy.stats
 from scipy.stats import shapiro
-import statsmodels.api as sm 
+import statsmodels.api as sm
+from scipy.stats import kruskal
 
 db = objects.Database(config.server, config.database, config.user, config.password)
 
@@ -30,28 +31,88 @@ def shapiro_wilk(univariate_data, note):
     else:
         print("\tShapiro-Wilk test of normality ({0}): The variable is probably NOT Gaussian (W = {1}, p={2})".format(note, str(round(stat,3)), str(round(p,3))))
 
+# Logistisk regression (https://www.geeksforgeeks.org/logistic-regression-using-statsmodels/).
+# Documentation: https://www.statsmodels.org/stable/generated/statsmodels.discrete.discrete_model.Logit.html
+def logreg(y, x):
+    log_reg = sm.Logit(y, x).fit() 
+    print(log_reg.summary())
+    input('Press any key to continue...')
+
+# Kruskal-Wallis H test (https://machinelearningmastery.com/statistical-hypothesis-tests-in-python-cheat-sheet/).
+# Documentation: https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.kruskal.html 
+def interpret_kruskal(stat, p):
+    print('H=%.3f, p=%.3f' % (stat, p))
+    if p > 0.05:
+        print('Accept NULL hypothesis - No significant difference between groups.')
+    else:
+        print('Reject NULL hypothesis - Significant differences exist between groups.')
+    input('Press any key to continue...')
+
 ############################################################################################################################################### Section 5.4.2 (Company attributes and implications of missing data).
 
-# https://www.statsmodels.org/stable/generated/statsmodels.discrete.discrete_model.Logit.html
-# https://www.geeksforgeeks.org/logistic-regression-using-statsmodels/
+def missing():
+    df = df_Master[['currentnumber', 'approvalnumber', 'propensity', 'netresult', 'established', 'employees']]
 
-df = df_Master[['currentnumber', 'approvalnumber', 'propensity', 'netresult', 'established', 'employees']]
+    # Defining the independent variables .
+    x = df[['currentnumber', 'approvalnumber', 'propensity']] 
+    
+    # Creating dummy (dependent) variables.
+    df.loc[~df['employees'].isnull(), 'dummy_employees'] = 1
+    df.loc[df['employees'].isnull(), 'dummy_employees'] = 0
 
-print(df.head(100))
+    df.loc[~df['established'].isnull(), 'dummy_established'] = 1
+    df.loc[df['established'].isnull(), 'dummy_established'] = 0
 
-# Creating dummy (dependent) variables.
-df.loc[~df['employees'].isnull(), 'dummy_employees'] = 1
-df.loc[df['employees'].isnull(), 'dummy_employees'] = 0
+    df.loc[~df['netresult'].isnull(), 'dummy_netresult'] = 1
+    df.loc[df['netresult'].isnull(), 'dummy_netresult'] = 0
 
-print(df.head(100))
+    # Running logistic regression model with dependent variables individually.
+    logreg(df[['dummy_employees']] , x)
+    logreg(df[['dummy_established']] , x)
+    logreg(df[['dummy_netresult']] , x)
 
-# defining the dependent and independent variables 
-x = df[['currentnumber', 'approvalnumber', 'propensity']] 
-y = df[['dummy_employees']] 
-   
-# building the model and fitting the data 
-log_reg = sm.Logit(y, x).fit() 
+#missing()
 
-print(log_reg.summary())
+def businessType():
+    df = df_Master[['currentnumber', 'approvalnumber', 'propensity', 'businesstype']]
 
-# Fortolkning: Større sandsynlighed for at en pågældende virksomhed har data om antal ansatte, jo færre elever den har ansat...
+    # Remove observations where business type is not either sole proprietorship or public/private limited.
+    df = df.loc[(df['businesstype'] == 'Aktieselskab') | (df['businesstype'] == 'Anpartsselskab') | (df['businesstype'] == 'Enkeltmandsvirksomhed')]
+
+    # Defining the independent variables .
+    x = df[['currentnumber', 'approvalnumber', 'propensity']] 
+
+    # Creating dummy (dependent) variables.
+    df.loc[df['businesstype'] == 'Aktieselskab', 'publiclimited'] = 1
+    df.loc[df['businesstype'] != 'Aktieselskab', 'publiclimited'] = 0
+
+    df.loc[(df['businesstype'] == 'Aktieselskab') | (df['businesstype'] == 'Anpartsselskab'), 'publicprivatelimited'] = 1
+    df.loc[(df['businesstype'] != 'Aktieselskab') & (df['businesstype'] != 'Anpartsselskab'), 'publicprivatelimited'] = 0
+
+    # Running logistic regression model with dependent variables individually.
+    logreg(df[['publiclimited']] , x)
+    logreg(df[['publicprivatelimited']] , x)
+
+#businessType()
+
+def businessSector():
+    df = df_Master[['currentnumber', 'approvalnumber', 'propensity', 'sectorcode', 'sectorname']]
+
+    # Sector codes.
+    sectorcode_frem = 251100
+    sectorcode_maskin = 256200
+    sectorcode_auto = 452010
+    sectorcode_detail = 451120
+
+    # Limit to the four most represented business sectors.
+    df = df.loc[(df['sectorcode'] == sectorcode_frem) | (df['sectorcode'] == sectorcode_maskin) | (df['sectorcode'] == sectorcode_auto) | (df['sectorcode'] == sectorcode_detail)]
+
+    # Analyse variance in currentnumber across sectors.
+    stat, p = kruskal(*[group["currentnumber"].values for name, group in df.groupby("sectorcode")])
+    interpret_kruskal(stat, p)
+
+    # Analyse variance in propensity across sectors.
+    stat, p = kruskal(*[group["propensity"].values for name, group in df.groupby("sectorcode")])
+    interpret_kruskal(stat, p)
+
+businessSector()
